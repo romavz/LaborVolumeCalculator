0d677c616a980c5a3573fe9f131edcdde5e56ef1
romavz <zworkbox@yandex.ru>
2020-05-19 16:23:51 +0300

    Добавление CRUD страниц для модели "Свойства новизны НИР"

---
+++ LaborVolumeCalculator/Pages/NirInnovationProperties/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LaborVolumeCalculator.Data;
+using LaborVolumeCalculator.Models;
+
+namespace LaborVolumeCalculator.Pages.NirInnovationProperties
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly LaborVolumeCalculator.Data.LVCContext _context;
+
+        public DeleteModel(LaborVolumeCalculator.Data.LVCContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public NirInnovationProperty NirInnovationProperty { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            NirInnovationProperty = await _context.NirInnovationProperties.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (NirInnovationProperty == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            NirInnovationProperty = await _context.NirInnovationProperties.FindAsync(id);
+
+            if (NirInnovationProperty != null)
+            {
+                _context.NirInnovationProperties.Remove(NirInnovationProperty);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ LaborVolumeCalculator/Pages/NirInnovationProperties/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LaborVolumeCalculator.Data;
+using LaborVolumeCalculator.Models;
+
+namespace LaborVolumeCalculator.Pages.NirInnovationProperties
+{
+    public class EditModel : PageModel
+    {
+        private readonly LaborVolumeCalculator.Data.LVCContext _context;
+
+        public EditModel(LaborVolumeCalculator.Data.LVCContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public NirInnovationProperty NirInnovationProperty { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            NirInnovationProperty = await _context.NirInnovationProperties.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (NirInnovationProperty == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(NirInnovationProperty).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NirInnovationPropertyExists(NirInnovationProperty.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool NirInnovationPropertyExists(int id)
+        {
+            return _context.NirInnovationProperties.Any(e => e.ID == id);
+        }
+    }
+}
