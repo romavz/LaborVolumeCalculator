2cd9298dadd091579f44df6683554677f7f2b049
romavz <zworkbox@yandex.ru>
2020-06-02 18:49:50 +0300

    Добавление в HtmlHelper расширения DisplayGroupNameFor, для вытягивания названия для группы объектов из аттрибутов модели

---
+++ LaborVolumeCalculator/Helpers/DisplayExtentions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace LaborVolumeCalculator.Helpers
+{
+    public static class DisplayExtentions
+    {
+        public static string DisplayGroupNameFor<TModel, TDataModel>(this IHtmlHelper<TModel> helper, Expression<Func<TModel, TDataModel>> expression) 
+                where TModel: class 
+                where TDataModel: class
+        {
+            Type modelType = typeof(TDataModel);
+            DisplayAttribute displayAttribute = GetDisplayAttributeFor(modelType);
+
+            string gruoupName = displayAttribute?.GetGroupName() ?? modelType.Name;
+
+            return gruoupName;
+        }
+
+        private static DisplayAttribute GetDisplayAttributeFor(Type modelType)
+        {
+            bool includeInherited = true;
+            object[] attributes = modelType.GetCustomAttributes(includeInherited);
+
+            return (DisplayAttribute)attributes.First(attr => attr is DisplayAttribute);
+        }
+    }
+}
