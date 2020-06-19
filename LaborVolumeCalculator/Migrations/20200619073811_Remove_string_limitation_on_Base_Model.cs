1b32c21e654e1a509b845a1d1e2766f2142ca39d
romavz <zworkbox@yandex.ru>
2020-06-19 10:46:18 +0300

    Отмена ограничения длинны названия объектов, наследников BasicModel.

---
+++ LaborVolumeCalculator/Migrations/20200619073811_Remove_string_limitation_on_Base_Model.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LaborVolumeCalculator.Migrations
+{
+    public partial class Remove_string_limitation_on_Base_Model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "NirInnovationProperty",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Nir",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "NirInnovationProperty",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Nir",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
