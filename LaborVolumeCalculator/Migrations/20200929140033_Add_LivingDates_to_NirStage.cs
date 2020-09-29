16cb86b8f92649c854fb7a0e3d404c8cdda1ad10
romavz <31279623+romavz@users.noreply.github.com>
2020-09-29 17:11:31 +0300

    add_living_dates_to_nirStage (#33)

---
+++ LaborVolumeCalculator/Migrations/20200929140033_Add_LivingDates_to_NirStage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LaborVolumeCalculator.Migrations
+{
+    public partial class Add_LivingDates_to_NirStage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateFrom",
+                schema: "Registers",
+                table: "NirStage",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateTo",
+                schema: "Registers",
+                table: "NirStage",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<double>(
+                name: "Volume",
+                schema: "Registers",
+                table: "NirStage",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateFrom",
+                schema: "Registers",
+                table: "NirStage");
+
+            migrationBuilder.DropColumn(
+                name: "DateTo",
+                schema: "Registers",
+                table: "NirStage");
+
+            migrationBuilder.DropColumn(
+                name: "Volume",
+                schema: "Registers",
+                table: "NirStage");
+        }
+    }
+}
