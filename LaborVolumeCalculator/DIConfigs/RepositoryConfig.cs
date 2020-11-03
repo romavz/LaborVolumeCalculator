fdc95a2fa3d29842b63b476a06d4ea847d875dae
romavz <zworkbox@yandex.ru>
2020-11-03 13:23:14 +0300

    Добавлен репозиторий для работ по разработке БД

--- LaborVolumeCalculator/DIConfigs/RepositoryConfig.cs
+++ LaborVolumeCalculator/DIConfigs/RepositoryConfig.cs
@@ -16,6 +16,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<IRepository<OntdLabor>, RepositoryBase<OntdLabor>>();
             services.AddScoped<IRepository<NirStageOntdLaborVolume>, RepositoryBase<NirStageOntdLaborVolume>>();
             services.AddScoped<IRepository<CorrectionRatesBundle>, CorrectionRatesBundleRepository>();
+            services.AddScoped<IRepository<DbDevLabor>, DbDevLaborRepository>();
             return services;
         }
     }
