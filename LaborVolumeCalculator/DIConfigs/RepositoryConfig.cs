2cf7bd456642f453fb8bc7433e015734cc89b215
romavz <zworkbox@yandex.ru>
2020-11-25 15:18:44 +0300

    Замена источника данных НИР репозиторием

--- LaborVolumeCalculator/DIConfigs/RepositoryConfig.cs
+++ LaborVolumeCalculator/DIConfigs/RepositoryConfig.cs
@@ -9,6 +9,7 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static IServiceCollection AddRepositories(this IServiceCollection services)
         {
+            services.AddScoped<IRepository<Nir>, NirRepository>();
             services.AddScoped<IRepository<NirStage>, NirStageRepository>();
             services.AddScoped<IRepository<NirStageLaborVolume>, RepositoryBase<NirStageLaborVolume>>();
             services.AddScoped<IRepository<NirStageSoftwareDevLaborGroup>, RepositoryBase<NirStageSoftwareDevLaborGroup>>();
