f6def78388211691aa2714fa2c734dca9f182749
romavz <zworkbox@yandex.ru>
2020-11-27 13:12:04 +0300

    Добавление объектов DTO для ArchitectureComplexityRate

--- LaborVolumeCalculator/DTO/ArchitectureComplexityRateDto.cs
+++ LaborVolumeCalculator/DTO/ArchitectureComplexityRateDto.cs
@@ -4,13 +4,21 @@ namespace LaborVolumeCalculator.DTO
     {
         public int ID { get; set; }
         public double Value { get; set; }
-
         public int ComponentsMakroArchitectureID { get; set; }
-
         public string ComponentsMakroArchitectureName { get; set; }
-
         public int ComponentsInteractionArchitectureID { get; set; }
-
         public string ComponentsInteractionArchitectureName { get; set; }
+    }
+
+    public class ArchitectureComplexityRateCreateDto
+    {
+        public double Value { get; set; }
+        public int ComponentsMakroArchitectureID { get; set; }
+        public int ComponentsInteractionArchitectureID { get; set; }
+    }
+
+    public class ArchitectureComplexityRateShortDto : ArchitectureComplexityRateCreateDto
+    {
+        public int ID { get; set; }
     }
 }
